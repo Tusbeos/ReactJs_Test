4e18f4ce327e711b2223e4aed7790988e627fb9f
Tusbeos <biconghi123@gmail.com>
2025-08-13 13:49:15 +0700

    create form login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -12,7 +12,46 @@ class Login extends Component {
   }
 
   render() {
-    return <div> Hello Login</div>;
+    return (
+      <div className="login_background">
+        <div className="login-container">
+          <div className="login-content">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 form-group input-login">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control login"
+                placeholder="Enter Your Username"
+              ></input>
+            </div>
+            <div className="col-12 form-group input-login">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter Your Password"
+              ></input>
+            </div>
+            <div className="col-12">
+              <button className="btn-login ">Login</button>
+            </div>
+            <div className="col-12 forgot-password;">
+              <span>Forgot your password!</span>
+            </div>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login text-center">
+                Or Login with:
+              </span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-google"></i>
+              <i class="fab fa-facebook-f"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
