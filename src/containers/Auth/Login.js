30ba86ef0f49d666503b224eb3d5ebdd0e67a30c
Tusbeos <biconghi123@gmail.com>
2025-08-14 11:35:24 +0700

    use state on react

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -9,44 +9,79 @@ import { FormattedMessage } from "react-intl";
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      password: "",
+      showPassword:false
+    }
   }
-
+handleOnChangeUsername = (event) => {
+  this.setState({
+      username: event.target.value,
+    });
+}
+handleOnChangePassword = (event) => {
+  this.setState({
+      password: event.target.value,
+    });
+}
+handleLogin = async () => {
+            // alert("Login button clicked");
+    console.log("Username:", this.state.username,"Password:", this.state.password);
+console.log("all State:", this.state);
+}
+handleOnChangeIconEyes = () => {
+  this.setState({
+    showPassword: !this.state.showPassword,
+  });
+}
   render() {
     return (
-      <div className="login_background">
-        <div className="login-container">
-          <div className="login-content">
-            <div className="col-12 text-login">Login</div>
-            <div className="col-12 form-group input-login">
+      <div className = "login_background">
+      <div className = "login-container">
+      <div className = "login-content">
+      <div className = "col-12 text-login">Login</div>
+      <div className = "col-12 form-group input-login">
               <label>Username</label>
+              <div >
               <input
-                type="text"
-                className="form-control login"
-                placeholder="Enter Your Username"
+                type        = "text"
+                className   = "form-control login"
+                placeholder = "Enter Your Username"
+                value       = {this.state.username}
+                onChange    = {(event) => this.handleOnChangeUsername(event)}
               ></input>
+              </div>
             </div>
-            <div className="col-12 form-group input-login">
+      <div className = "col-12 form-group input-login">
               <label>Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter Your Password"
+              <div className = "custom-input-password">
+                  <input
+                type        = {this.state.showPassword ? "text" : "password"}
+                className   = "form-control"
+                placeholder = "Enter Your Password"
+                value       = {this.state.password}
+                onChange    = {(event) => this.handleOnChangePassword(event)}
               ></input>
+              <span onClick = {() => this.handleOnChangeIconEyes()}>
+                <i className = {this.state.showPassword ?  "fas fa-eye-slash" : "fas fa-eye"}></i>
+              </span>
+              </div>
+      </div>
+            <div    className = "col-12">
+            <button className = "btn-login" onClick = {() => this.handleLogin()} >Login</button>
             </div>
-            <div className="col-12">
-              <button className="btn-login ">Login</button>
-            </div>
-            <div className="col-12 forgot-password;">
+            <div className = "col-12 forgot-password;">
               <span>Forgot your password!</span>
             </div>
-            <div className="col-12 text-center mt-3">
-              <span className="text-other-login text-center">
-                Or Login with:
+            <div  className = "col-12 text-center mt-3">
+            <span className = "text-other-login text-center">
+                Or Login with: 
               </span>
             </div>
-            <div className="col-12 social-login">
-              <i className="fab fa-google"></i>
-              <i class="fab fa-facebook-f"></i>
+            <div className = "col-12 social-login">
+            <i   className = "fab fa-google"></i>
+            <i   class     = "fab fa-facebook-f"></i>
             </div>
           </div>
         </div>
@@ -63,7 +98,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    navigate: (path) => dispatch(push(path)),
+    navigate         : (path) => dispatch(push(path)),
     adminLoginSuccess: (adminInfo) =>
       dispatch(actions.adminLoginSuccess(adminInfo)),
     adminLoginFail: () => dispatch(actions.adminLoginFail()),
