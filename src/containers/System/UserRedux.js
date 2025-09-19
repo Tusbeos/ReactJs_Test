03d87b2a7bc4cb0c5271a5ba9e46174136c8e308
Tusbeos <biconghi123@gmail.com>
2025-09-19 12:03:07 +0700

    fix header admin

---
+++ src/containers/System/UserRedux.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+class UserRedux extends Component {
+    constructor(props) {
+        super(props)
+            this.state={
+
+            }
+    }
+    componentDidMount() {
+    }
+
+
+    render() {
+        return (
+            <div className='user-redux-container'>
+                <div className='title'>User Manage Redux</div>
+                <div className='user-redux-body'></div>
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
