0e95b2d394d2661119381f494a05963edf5ab034
Tusbeos <biconghi123@gmail.com>
2025-09-25 20:53:14 +0700

    loading gender from redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,16 +2,15 @@ import {combineReducers} from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import appReducer from "./appReducer";
-import adminReducer from "./adminReducer";
 import userReducer from "./userReducer";
-
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import adminReducer from "./adminReducer";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 const persistCommonConfig = {
-    storage: storage,
-    stateReconciler: autoMergeLevel2,
+  storage: storage,
+  stateReconciler: autoMergeLevel2,
 };
 const appPersistConfig = {
   ...persistCommonConfig,
@@ -30,4 +29,5 @@ export default (history) =>
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
     app: persistReducer(appPersistConfig, appReducer),
+    admin: adminReducer,
   });
